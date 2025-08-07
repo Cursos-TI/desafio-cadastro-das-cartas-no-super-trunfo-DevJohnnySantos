eccc45756346b3cc8204d75a2b267e3a80b26616
Jonathan Santos <142257912+DevJohnnySantos@users.noreply.github.com>
2025-08-07 18:17:03 +0000

    correcao do erro de buffer e cadastro da segunda carta

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -21,7 +21,7 @@ int main() {
     scanf("%d",&codCarta );
 
     printf("informe o nome da cidade:\n");
-    scanf("%[^\n]s", nomeCid);
+    scanf(" %[^\n]s", nomeCid);
 
     printf("informe a população da cidade:\n");
     scanf("%d", &populacao);
@@ -35,7 +35,43 @@ int main() {
     printf("inforem quantos pontos turisticos:\n");
     scanf("%d", &pontoTuri);
 
-    printf("\n--- Dados da Carta ---\n");
+     printf("\n--- Dados da Carta ---\n");
+    printf("Estado (UF): %s\n", estado);
+    printf("Codigo da Carta: %d\n", codCarta);
+    printf("Cidade: %s\n", nomeCid);
+    printf("Populacao: %d\n", populacao);
+    printf("Area em Km2: %.2f\n", areaKm2);
+    printf("PIB: %.2f\n", pib);
+    printf("Pontos Turisticos: %d\n", pontoTuri);
+
+
+
+    printf("Informe os dados da segunda carta\n");
+
+    
+    printf("informe o UF do estado: \n");
+    scanf("%s", estado);
+
+
+    printf("informe o codigo da carta!\n");
+    scanf("%d",&codCarta );
+
+    printf("informe o nome da cidade:\n");
+    scanf(" %[^\n]s", nomeCid);
+
+    printf("informe a população da cidade:\n");
+    scanf("%d", &populacao);
+
+    printf("informe o Km2 do estado:\n");
+    scanf("%f", &areaKm2);
+
+    printf("informe o PIB do estado:\n");
+    scanf("%f", &pib);
+
+    printf("inforem quantos pontos turisticos:\n");
+    scanf("%d", &pontoTuri);
+
+    printf("\n--- Dados da segunda Carta ---\n");
     printf("Estado (UF): %s\n", estado);
     printf("Codigo da Carta: %d\n", codCarta);
     printf("Cidade: %s\n", nomeCid);
